3822f0516f0eb3107ef2a29a00257d45449b55fb
Kévin Felipe <devkevinfelip@gmail.com>
2022-03-28 16:53:26 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Titulo"),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+        actions: <Widget>[
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.refresh),
+          ),
+        ],
+      ),
+    );
+  }
+}
