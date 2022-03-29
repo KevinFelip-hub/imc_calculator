b61ddebd13610626f4bd3a7ddbc14be40c4745c8
Kévin Felipe <devkevinfelip@gmail.com>
2022-03-29 17:59:43 -0300

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -26,13 +26,60 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Titulo"),
+        title: const Text("Calculadora IMC"),
         centerTitle: true,
-        backgroundColor: Colors.black,
+        backgroundColor: Colors.grey,
         actions: <Widget>[
           IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.refresh),
+            onPressed: () {},
+            icon: const Icon(Icons.refresh),
+          ),
+        ],
+      ),
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Icon(
+            Icons.person_outline,
+            color: Colors.grey,
+            size: 120.0,
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: "Peso (Kg)",
+              labelStyle: TextStyle(color: Colors.grey),
+            ),
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.grey,
+              fontSize: 20.0,
+            ),
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: "Altura (cm)",
+              labelStyle: TextStyle(color: Colors.grey),
+            ),
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.grey,
+              fontSize: 20.0,
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text(
+              "Calcular",
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            style: ElevatedButton.styleFrom(
+              primary: Colors.grey,
+            ),
           ),
         ],
       ),
